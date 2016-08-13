f9af35d25baf3dd504ff40d67043ca640920cb56
Dean C. Siroky <Dean C. Siroky>
2016-08-13 02:24:08 -0400

    Fixed Various Issues

--- app/src/main/java/com/example/deanc/gps_alarm/DestinationAddress.java
+++ app/src/main/java/com/example/deanc/gps_alarm/DestinationAddress.java
@@ -4,10 +4,27 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import butterknife.Bind;
 
@@ -16,16 +33,24 @@ import butterknife.Bind;
  */
 public class DestinationAddress extends DialogFragment{
 
-    @Bind(R.id.street_entry) EditText street;
-    @Bind(R.id.city_entry) EditText city;
-    @Bind(R.id.state_entry) EditText state;
-    @Bind(R.id.zip_entry) EditText zip;
+    protected static final String HTTP = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    protected static final String API_KEY = "&key=AIzaSyDvSzZs2vIJzot6RrRfPwlBWStLLTrkijY";
+
+    EditText street, city, state, zip;
+    LatLng userDestination;
+    Double destinationLat, destinationLon;
+    String URL, user_address;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         LayoutInflater i = getActivity().getLayoutInflater();
         View v = i.inflate(R.layout.destination_address, null);
+
+        street = (EditText) v.findViewById(R.id.street_entry);
+        city = (EditText) v.findViewById(R.id.city_entry);
+        state = (EditText) v.findViewById(R.id.state_entry);
+        zip = (EditText) v.findViewById(R.id.zip_entry);
 
         AlertDialog.Builder b;
         b = new AlertDialog.Builder(getActivity());
@@ -43,7 +68,10 @@ public class DestinationAddress extends DialogFragment{
                                 z = zip.getText().toString();
 
                                 full_address = strt + ",+" + c + ",+" + sta + ",+" + z;
-                                MainActivity.user_address = full_address.replace(' ', '+');
+                                user_address = full_address.replace(' ', '+');
+
+                                URL = HTTP + user_address + API_KEY;
+                                new getAddressCoordinates().execute(URL);
 
                                 dialog.dismiss();
                             }
@@ -52,4 +80,87 @@ public class DestinationAddress extends DialogFragment{
 
         return b.create();
     }
+
+    private class getAddressCoordinates extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            StringBuilder sb = new StringBuilder();
+
+            HttpURLConnection urlConnection = null;
+            try {
+                java.net.URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.connect();
+
+                int HttpResult = urlConnection.getResponseCode();
+                if (HttpResult == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(
+                            urlConnection.getInputStream(), "utf-8"));
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    in.close();
+
+                    //System.out.println("" + sb.toString());
+                    return sb.toString();
+
+                } else {
+                    System.out.println(urlConnection.getResponseMessage());
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+
+                JSONObject json = new JSONObject(result);
+                JSONArray results = json.getJSONArray("results");
+                JSONObject components = results.getJSONObject(0);
+                JSONObject geometry = components.getJSONObject("geometry");
+                JSONObject location = geometry.getJSONObject("location");
+
+                String lat = location.getString("lat");
+                String lon = location.getString("lon");
+
+                destinationLat = Double.parseDouble(lat);
+                destinationLon = Double.parseDouble(lon);
+
+                userDestination = new LatLng(destinationLat,destinationLon);
+
+                MainActivity.mapFrag.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap map) {
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userDestination, 17));
+
+                        // Markers identify locations on the map.
+                        map.addMarker(new MarkerOptions()
+                                .title("Your Destination")
+                                .position(userDestination));
+                    }
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/deanc/gps_alarm/MainActivity.java
+++ app/src/main/java/com/example/deanc/gps_alarm/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -39,8 +40,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected static final String HTTP = "https://maps.googleapis.com/maps/api/geocode/json?address=";
     protected static final String API_KEY = "&key=AIzaSyDvSzZs2vIJzot6RrRfPwlBWStLLTrkijY";
 
-    @Bind(R.id.destination) Button destination;
-    @Bind(R.id.start_tracking) Button start_tracking;
+    public static MapFragment mapFrag;
+    Button destination, start_tracking;
     Tracker gpsTracker;
 
     String URL;
@@ -54,59 +55,68 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
+        // userLat = 40.785227;
+        // userLon = -73.673233;
+
+        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        destination = (Button) findViewById(R.id.destination);
+        start_tracking = (Button) findViewById(R.id.start_tracking);
         start_tracking.setEnabled(false);
 
-//        gpsTracker = new Tracker(MainActivity.this);
-//
-//        if (gpsTracker.canGetLocation()) {
-//
-//            userLat = gpsTracker.getLatitude();
-//            userLon = gpsTracker.getLongitude();
-//
-//            userLocation = new LatLng(userLat,userLon);
-//
-//        } else {
-//            // can't get location
-//            // GPS or Network is not enabled
-//            // Ask user to enable GPS/network in settings
-//            gpsTracker.showSettingsAlert();
-//        }
+        mapFrag.getMapAsync(this);
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        // userLocation = new LatLng(userLat,userLon);
+
+        gpsTracker = new Tracker(MainActivity.this);
+
+        if (gpsTracker.canGetLocation()) {
+
+            userLat = gpsTracker.getLatitude();
+            userLon = gpsTracker.getLongitude();
+
+            userLocation = new LatLng(userLat,userLon);
+
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gpsTracker.showSettingsAlert();
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, (long) 1000, (float) 10, new LocationListener() {
 
-            @Override
-            public void onLocationChanged(Location location) {
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-
-        });
-        Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-        userLat = location.getLatitude();
-        userLon = location.getLongitude();
-
-        userLocation = new LatLng(userLat,userLon);
+//        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, (long) 1000, (float) 10, new LocationListener() {
+//
+//            @Override
+//            public void onLocationChanged(Location location) {
+//
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderEnabled(String provider) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderDisabled(String provider) {
+//
+//            }
+//
+//        });
+//        Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+//        userLat = location.getLatitude();
+//        userLon = location.getLongitude();
+//
+//         userLocation = new LatLng(userLat,userLon);
 
 
 
@@ -122,8 +132,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         start_tracking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                URL = HTTP + user_address + API_KEY;
-                new getAddressCoordinates().execute(URL);
+
 
             }
         });
@@ -141,10 +150,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         map.addMarker(new MarkerOptions()
                 .title("You are Here")
                 .position(userLocation));
-
-//        map.addMarker(new MarkerOptions()
-//                .title("Destination")
-//                .position(userDestination));
 
     }
 
@@ -176,75 +181,5 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         return value * Math.PI / 180;
     }
 
-    private class getAddressCoordinates extends AsyncTask<String, String, String> {
 
-        @Override
-        protected String doInBackground(String... params) {
-
-            StringBuilder sb = new StringBuilder();
-
-            HttpURLConnection urlConnection = null;
-            try {
-                java.net.URL url = new URL(params[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setUseCaches(false);
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setReadTimeout(10000);
-                urlConnection.connect();
-
-                int HttpResult = urlConnection.getResponseCode();
-                if (HttpResult == HttpURLConnection.HTTP_OK) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(
-                            urlConnection.getInputStream(), "utf-8"));
-                    String line;
-                    while ((line = in.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                    in.close();
-
-                    //System.out.println("" + sb.toString());
-                    return sb.toString();
-
-                } else {
-                    System.out.println(urlConnection.getResponseMessage());
-                }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (urlConnection != null)
-                    urlConnection.disconnect();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            try {
-
-                JSONObject json = new JSONObject(result);
-                JSONArray results = json.getJSONArray("results");
-                JSONObject components = results.getJSONObject(0);
-                JSONObject geometry = components.getJSONObject("geometry");
-                JSONObject location = geometry.getJSONObject("location");
-
-
-//                for (int i = 0; i < address.length(); i++) {
-//
-//                    JSONObject values = address.getJSONObject(i);
-//                    String key = values.getJSONArray("types").get(0).toString();
-//                    String value = values.getString("long_name");
-//                   // address_components.put(key,value);
-//                    android.util.Log.d("Dean", key + " " + value);
-//                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
 }
